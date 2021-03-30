20db6ffb9f5c029ed85b2ef0a16712a4a478a41e
hmm-shnchr <homma.shinichiro@gmail.com>
2021-03-30 22:20:13 +0900

    modified: Modified the Adabound.

--- optimizer_gpu.py
+++ optimizer_gpu.py
@@ -1,5 +1,18 @@
 import cupy as np
 
+
+def set_optimizer(opt, lr):
+    if opt == "SGD":
+        return SGD(lr = lr)
+    if opt == "Adam":
+        return Adam(lr = lr)
+    if opt == "AdaBound":
+        return AdaBound(lr = lr, final_lr = 0.1)
+    else:
+        print("{} is not defined.".format(opt))
+        return None
+    
+
 class SGD:
     def __init__(self, lr = 0.1):
         self.lr = lr
@@ -40,7 +53,6 @@ class AdaBound:
         self.final_lr = final_lr
         self.beta1 = 0.9
         self.beta2 = 0.999
-        self.gamma = 0.001
         self.iters = 0
         self.m = None
         self.v = None
@@ -54,23 +66,10 @@ class AdaBound:
                 self.m[key] = np.zeros_like(val)
                 self.v[key] = np.zeros_like(val)
         self.iters += 1
-        lower_lr = self.final_lr * (1.0 - 1.0 / ((1.0-self.gamma) * self.iters + 1.0))
-        higher_lr = self.final_lr * (1.0 + 1.0 / ((1.0-self.gamma) * self.iters))
-        lr = self.lr * np.sqrt(1.0 - self.beta2**self.iters) / (1.0 - self.beta1**self.iters)
+        lower_lr = self.final_lr * (1.0 - 1.0 / ((1.0-self.beta2) * self.iters + 1.0))
+        higher_lr = self.final_lr * (1.0 + 1.0 / ((1.0-self.beta2) * self.iters))
         for key in params.keys():
             self.m[key] += (1.0 - self.beta1) * (grads[key] - self.m[key])
             self.v[key] += (1.0 - self.beta2) * (grads[key]**2 - self.v[key])
-            lr_t = np.clip(lr / (np.sqrt(self.v[key]) + self.eps), lower_lr, higher_lr)
+            lr_t = np.clip(self.lr / (np.sqrt(self.v[key]) + self.eps), lower_lr, higher_lr) / np.sqrt(self.iters)
             params[key] -= lr_t * self.m[key]
-
-
-def set_optimizer(opt, lr):
-    if opt == "SGD":
-        return SGD(lr = lr)
-    if opt == "Adam":
-        return Adam(lr = lr)
-    if opt == "AdaBound":
-        return AdaBound(lr = lr, final_lr = 0.1)
-    else:
-        print("{} is not defined.".format(opt))
-        return None
